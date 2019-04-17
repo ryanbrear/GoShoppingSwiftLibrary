27397787e050a64de386d60f0a33c01518dc3599
ryanbrear <ryan@godochurch.com>
2019-04-17 14:08:15 +0200

    Initial Commit

---
+++ GoShoppingSwift/GoShoppingSwift.h
@@ -0,0 +1,19 @@
+//
+//  GoShoppingSwift.h
+//  GoShoppingSwift
+//
+//  Created by Ryan Brear on 2019/04/17.
+//  Copyright © 2019 Ryan Brear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoShoppingSwift.
+FOUNDATION_EXPORT double GoShoppingSwiftVersionNumber;
+
+//! Project version string for GoShoppingSwift.
+FOUNDATION_EXPORT const unsigned char GoShoppingSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoShoppingSwift/PublicHeader.h>
+
+
